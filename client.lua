f0ceb724219ade04446c549558e548d1971fee7b
Reaverr <57047754+Reaverr@users.noreply.github.com>
2019-11-29 22:20:00 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,25 @@
+--[Coded By Reaverr]
+--[בשביל להגדיר את כמות החיים שהנשק יוריד צריך לשנות את המספר שיש למטה]
+--[0.0-1 טווח המספרים הוא]
+
+Citizen.CreateThread(function()
+    while true do
+	    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.2)
+        N_0x4757f00bc6323cfe(GetHashKey("WEAPON_BAT"), 0.4)
+    	N_0x4757f00bc6323cfe(GetHashKey("WEAPON_KNIFE"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_NIGHTSTICK"), 0.3)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SWITCHBLADE"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_KNUCKLE"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_GOLFCLUB"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_HAMMER"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_CROWBAR"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_BOTTLE"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_DAGGER"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_HATCHET"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_MACHETE"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_FLASHLIGHT"), 0.1)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_PIPEWRENCH"), 0.5)
+		N_0x4757f00bc6323cfe(GetHashKey("WEAPON_SMOKEGRENADE"), 0.1)
+		Wait(0)
+	end
+end)
